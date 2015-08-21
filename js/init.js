319ce304839ad8f2777e409fad8849d2ee3c9968
unknown <gbs03602@SVILP211.SG.GBS.PRO>
2015-08-21 18:30:44 +0530

    initial commit

---
+++ js/init.js
@@ -0,0 +1,7 @@
+(function(){
+	'use strict';
+	var app = angular.module('e2c-connexions', ['ngMaterial', 'ngRoute', 'controllers', 'services']);
+	app.config(function ($routeProvider) {
+		
+	});
+})();
---
+++ js/services.js
@@ -0,0 +1,10 @@
+(function () {
+	'use strict';
+	var app = app.module('services', [])
+	app.factory('api', ['$http', '$log', apiFn]);
+	function apiFn($http, $log) {
+		return {
+			
+		};
+	}
+})();
